0877f554d5445e976fd455a3e5c1a2f2815aab95
Jesus Mendoza Martinez <jsmendoza1312@gmail.com>
2023-12-23 00:10:52 -0500

    menu bootstrap finish

--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@fortawesome/fontawesome-free/css/all.min.css'
-import 'boostrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
 
 createApp(App).use(store).use(router).mount('#app')
 
 
 
-import 'boostrap/dist/js/boostrap'
+import 'bootstrap/dist/js/bootstrap'
