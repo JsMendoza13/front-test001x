9196af8ccc8e25b80299c1d2921900b04764ccbf
Jesus Mendoza Martinez <jsmendoza1312@gmail.com>
2023-12-23 00:57:53 -0500

    conect with api-students

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,60 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="row">
+    <div class="col-lg-8 offset-lg-2">
+      <div class="table-responsive">
+        <table class="table table-bordered table-hover">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>ID</th>
+              <th>FOTO</th>
+              <th>NOMBRE</th>
+              <th>APELLIDO</th>
+              <th>REGISTRO</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody class="table-group divider" id="contenido">
+            <tr v-if="this.cargando">
+              <td colspan="6"><h3>Cargando...</h3></td>
+            </tr>
+            <tr v-else v-for="(est, i) in this.estudiantes" :key="est.id">
+              <td v-text="i + 1"></td>
+              <td v-text="est.id"></td>
+              <td v-text="est.foto"></td>
+              <td v-text="est.nombre"></td>
+              <td v-text="est.apellido"></td>
+              <td v-text="est.created_at"></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      estudiantes: null,
+      cargando: false,
+    };
+  },
+  mounted() {
+    this.getEstudiantes();
+  },
+  methods: {
+    getEstudiantes() {
+      this.cargando = true;
+      axios.get("http://test.test/api/v1/estudiantes").then((res) => {
+        this.estudiantes = res.data;
+        this.cargando = false;
+      });
+    },
+  },
+};
 </script>
