6c6544549ebe8c5ebb92ab11ee957b1bd050dd32
Jesus Mendoza Martinez <jsmendoza1312@gmail.com>
2023-12-22 23:32:39 -0500

    config router

--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'boostrap/dist/css/bootstrap.css'
+
 
 createApp(App).use(store).use(router).mount('#app')
+
+
+
+import 'boostrap/dist/js/boostrap'
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import EstudianteNew from '../views/EstudianteNew.vue'
+import EstudianteEdit from '../views/EstudianteEdit.vue'
+import EstudianteView from '../views/EstudianteView.vue'
+
+
 
 const routes = [
   {
@@ -8,12 +13,19 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/createE',
+    name: 'create',
+    component: EstudianteNew
+  },
+  {
+    path: '/editE/:id',
+    name: 'editE',
+    component: EstudianteEdit
+  },
+  {
+    path: '/viewE/:id',
+    name: 'viewE',
+    component: EstudianteView
   }
 ]
 
