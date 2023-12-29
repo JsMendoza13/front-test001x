14f33e39c657b97a4d5a0fc30edf5cd58abc23e8
Jesus Mendoza Martinez <jsmendoza1312@gmail.com>
2023-12-29 00:57:15 -0500

    nothing new

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,3 +3,31 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+document.addEventListener("click", (e) => {
+  if (e.target.parentElement.classList.contains("tab-title")) {
+    redTab(e);
+  }
+});
+
+[...document.querySelectorAll("div.red-tab div")].forEach((tabTile, index) => {
+  tabTile.classList.toggle("active", index == 0);
+});
+[...document.querySelectorAll(".tab-content")].forEach((tabcontent, index) => {
+  tabcontent.classList.toggle("active", index == 0);
+});
+function redTab(e) {
+  let tabTiles = [...document.querySelectorAll("div.red-tab div")];
+  let tabcontents = [...document.querySelectorAll(".tab-content")];
+  let activeTabIndex = tabTiles.findIndex((tab) => {
+    return tab == e.target.parentElement;
+  });
+  tabTiles.forEach((tabTile, index) => {
+    tabTile.classList.toggle("active", index === activeTabIndex);
+  });
+  tabcontents.forEach((tabcontent, index) => {
+    tabcontent.classList.toggle("active", index === activeTabIndex);
+  });
+}
+</script>
